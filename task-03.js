15b067846f534bede02d069c01aacaa8e35bfcb4
Makson9556 <maksimmankovsk@gmail.com>
2025-03-20 18:38:15 +0200

    hw

---
+++ task-03.js
@@ -0,0 +1,36 @@
+// Напиши клас Storage, який буде створювати об'єкти для управління складом товарів. При виклику буде отримувати один аргумент - початковий масив товарів, і записувати його у властивість items.
+
+class Storage {
+    constructor(items) {
+      this.items = items;
+    }
+  
+    getItems() {
+      return this.items;
+    }
+  
+    addItem(item) {
+      this.items.push(item);
+    }
+  
+    removeItem(item) {
+      this.items = this.items.filter(currentItem => currentItem !== item);
+    }
+  }
+
+
+const storage = new Storage([
+    'Нанітоіди',
+    'Пролонгер',
+    'Залізні жупи',
+    'Антигравітатор',
+   ]);
+   
+   const items = storage.getItems();
+   console.table(items); // [ "Нанітоіди", "Пролонгер", "Залізні жупи", "Антигравітатор" ]
+   
+   storage.addItem('Дроїд');
+   console.table(storage.items); // [ "Нанітоіди", "Пролонгер", "Залізні жупи", "Антигравітатор", "Дроїд" ]
+   
+   storage.removeItem('Пролонгер');
+   console.table(storage.items); // [ "Нанітоіди", "Залізні жупи", "Антигравітатор", "Дроїд" ]
